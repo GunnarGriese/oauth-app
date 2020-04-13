a95b556d1e59bfbcee8090e8b7e5ef5603e6cd56
IIH Nordic <gunnargriese@gmx.de>
2020-04-13 20:32:23 +0200

    exclude g drive

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import google.oauth2.credentials
 import googleapiclient.discovery
 
 import google_auth
-import google_drive
 import google_sheets
 import causal_impact
 
@@ -19,7 +18,6 @@ app = flask.Flask(__name__)
 app.secret_key = os.environ.get("FN_FLASK_SECRET_KEY", default=False)
 
 app.register_blueprint(google_auth.app)
-app.register_blueprint(google_drive.app)
 app.register_blueprint(google_sheets.app)
 app.register_blueprint(causal_impact.app)
 
