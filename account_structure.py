aea6084303b5a6937e7d182f530ed0ce38165376
IIH Nordic <gunnargriese@gmx.de>
2020-04-30 18:56:27 +0200

    build classes for apis

--- account_structure.py
+++ account_structure.py
@@ -9,7 +9,10 @@ import google_sheets
 from werkzeug.utils import secure_filename
 import pandas as pd
 import requests
-from utils import Filter, response2df
+import plotly.graph_objects as go
+import plotly
+import json
+from utils import Filter, response2df, AnalyticsReporting, AnalyticsManagement
 
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 
@@ -43,6 +46,14 @@ def get_property(api_service, account_id, property_id):
         webPropertyId=property_id).execute()
     return prop
 
+def get_view_filters(mgmt_api, account_id, property_id, view_id):
+    response = mgmt_api.management().profileFilterLinks().list(
+      accountId=account_id,
+      webPropertyId=property_id,
+      profileId=view_id).execute()
+    view_filters = response.get('items', [])
+    return view_filters
+
 def handle_filters(filter_list):
     filter_dict = {}
     for idx, filter in enumerate(filter_list):
@@ -65,40 +76,51 @@ def handle_filters(filter_list):
 
     return filter_dict
 
-def get_demographics_report(api_service, VIEW_ID):
-  report = api_service.reports().batchGet(
-      body={
-        'reportRequests': [
-        {
-          'viewId': VIEW_ID,
-          'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
-          'metrics': [{'expression': 'ga:sessions'}],
-          'dimensions': [{'name': 'ga:userGender'},
-                        {'name': 'ga:userAgeBracket'}],
-          "pageSize": 100000
-        }]
-      }
-  ).execute()
-  df = response2df(report)
-  df["ga:sessions"] = df["ga:sessions"].astype('int64')
-  df['perc'] = df['ga:sessions'] / df['ga:sessions'].sum()
-  df.columns = ['Gender', 'Age Bucket', 'Sessions', 'Session Share']
-  return df
+def get_demographics_report(analytics):
+    df = analytics.report(
+        dimensions=[{'name': 'ga:userGender'}, {'name': 'ga:userAgeBracket'}],
+        metrics=[{'expression': 'ga:sessions'}],
+        date_range=['30daysAgo', 'today']
+        )
+    df["ga:sessions"] = df["ga:sessions"].astype('int64')
+    df.columns = ['Gender', 'Age Bucket', 'Sessions']
+    return df
 
-def get_hits_report(api_service, VIEW_ID):
-  report = api_service.reports().batchGet(
-      body={
-        'reportRequests': [
-        {
-          'viewId': VIEW_ID,
-          'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
-          'metrics': [{'expression': 'ga:hits'}],
-          'dimensions': [{'name': 'ga:date'}],
-          "pageSize": 100000
-        }]
-      }
-  ).execute()
-  df = response2df(report)
+
+def create_piechart(df):
+    """Create a plotly figure
+    Parameters
+    ----------
+    df : pandas.Dataframe
+    Returns
+    -------
+    fig1, fig2:
+        Plotly bar chart
+    """
+    # Count class occurences
+    df_female = df[df['Gender']=='female'].sort_values(by='Sessions', ascending=False)
+    df_male = df[df['Gender']=='male'].sort_values(by='Sessions', ascending=False)
+
+    layout = go.Layout(
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        legend=dict(
+            font=dict(
+                family="sans-serif",
+                color="white")
+                )
+            )
+    fig_female = go.Figure(data=[go.Pie(labels=df_female['Age Bucket'], values=df_female['Sessions'])], layout=layout)
+    fig_male = go.Figure(data=[go.Pie(labels=df_male['Age Bucket'], values=df_male['Sessions'])], layout=layout)
+
+    return fig_female, fig_male
+
+def get_hits_report(analytics):
+  df = analytics.report(
+        dimensions=[{'name': 'ga:date'}],
+        metrics=[{'expression': 'ga:hits'}],
+        date_range=['30daysAgo', 'today']
+        )
   df["ga:hits"] = df["ga:hits"].astype('int64')
   hits_greater = {}
   hits_greater['hit_count'] = df['ga:hits'].sum()
@@ -108,17 +130,24 @@ def get_hits_report(api_service, VIEW_ID):
 @app.route('/ga-account-structure', methods=['GET', 'POST'])
 def google_audit():
     mgmt_api = build_mgmt_service()
-    reporting_api = build_reporting_service()
+    #reporting_api = build_reporting_service()
     req_data = flask.request.form
     print(flask.request.method)
     if flask.request.method == "POST":
+        analytics = AnalyticsReporting(view_id=req_data['view'])
+        mgmt = AnalyticsManagement(req_data['account'], req_data['property'], req_data['view'])
+        #us = mgmt.list_account_users()
+        mgmt.get_property()
         accounts = get_accounts(mgmt_api)
         users = get_account_users(mgmt_api, req_data['account'])
         filters = get_account_filters(mgmt_api, req_data['account'])
         filter_data = handle_filters(filters)
         data_retention = get_property(mgmt_api, req_data['account'], req_data['property'])
-        demographics_df = get_demographics_report(reporting_api, req_data['view'])
-        hits_greater = get_hits_report(reporting_api, req_data['view'])
+        demographics_df = get_demographics_report(analytics)
+        fig_female, fig_male = create_piechart(demographics_df)
+        graphs = [fig_female, fig_male]
+        graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+        hits_greater = get_hits_report(analytics)
         for account in accounts['items']:
             if account['id'] == req_data['account']:
                 return flask.render_template('account_structure.html', 
@@ -128,6 +157,8 @@ def google_audit():
                                                 filters=filter_data,
                                                 demographics=[demographics_df.to_html(classes='table')],
                                                 hits_greater = hits_greater,
-                                                data_retention=data_retention)
+                                                data_retention=data_retention,
+                                                ids=['Female', 'Male'],
+                                                graphJSON=graphJSON)
     
     return flask.render_template('account_structure.html', user_info=google_auth.get_user_info())
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import google_auth
+import googleapiclient.discovery
 
 class Filter:
     def __init__(self, f_id,f_name, f_type, f_update, f_details=None):
@@ -18,16 +20,16 @@ def response2df(report):
     header_row = []
 
     #Get column headers, metric headers, and dimension headers.
-    columnHeader = report.get('columnHeader', {})
+    column_header = report.get('columnHeader', {})
     
-    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+    metric_headers = column_header.get('metricHeader', {}).get('metricHeaderEntries', [])
     
-    dimensionHeaders = columnHeader.get('dimensions', [])
+    dimension_headers = column_header.get('dimensions', [])
 
     #Combine all of those headers into the header_row, which is in a list format
-    for dheader in dimensionHeaders:
+    for dheader in dimension_headers:
         header_row.append(dheader)
-    for mheader in metricHeaders:
+    for mheader in metric_headers:
         header_row.append(mheader['name'])
 
     #Get data from each of the rows, and append them into a list
@@ -50,3 +52,131 @@ def response2df(report):
     result_df = pd.DataFrame(data_csv, columns=header_row)
 
     return result_df
+
+
+class AnalyticsReporting:
+    API_VER = 'v4'
+    API_NAME = 'analyticsreporting'
+
+    def __init__(self, view_id):
+        credentials = google_auth.build_credentials()
+        self.analytics = googleapiclient.discovery.build(self.API_NAME, self.API_VER, credentials=credentials)
+        self.view_id = view_id
+
+    def _get_report_data(self, dimensions, metrics, date_range):
+        reportRequests = [
+            {
+                "viewId": self.view_id,
+                "dateRanges": [{
+                    "startDate": date_range[0],
+                    "endDate": date_range[1],
+                }],
+                'metrics': metrics,
+                'dimensions': dimensions
+            }]
+        return self.analytics.reports().batchGet(
+            body={
+                'reportRequests': reportRequests
+            }
+        ).execute()
+
+    def report(self, dimensions, metrics, date_range):
+        """Parses and and returns a pd.DataFrame of the Analytics Reporting API V4 response"""
+        report = self._get_report_data(dimensions=dimensions, metrics=metrics, date_range=date_range)
+        report = report.get('reports', [])[0]
+        #Initialize empty data container for the two date ranges (if there are two that is)
+        data = []
+        header_row = []
+
+        #Get column headers, metric headers, and dimension headers.
+        column_header = report.get('columnHeader', {})
+        metric_headers = column_header.get('metricHeader', {}).get('metricHeaderEntries', [])
+        dimension_headers = column_header.get('dimensions', [])
+
+        #Combine all of those headers into the header_row, which is in a list format
+        for dheader in dimension_headers:
+            header_row.append(dheader)
+        for mheader in metric_headers:
+            header_row.append(mheader['name'])
+
+        #Get data from each of the rows, and append them into a list
+        rows = report.get('data', {}).get('rows', [])
+    
+        for row in rows:
+            row_temp = []
+            dimensions = row.get('dimensions', [])
+            metrics = row.get('metrics', [])
+        
+            for d in dimensions:
+                row_temp.append(d)
+            
+            for m in metrics[0]['values']:
+                row_temp.append(m)
+            
+            data.append(row_temp)
+    
+        #Putting those list formats into pandas dataframe, and append them into the final result
+        result_df = pd.DataFrame(data, columns=header_row)
+        return result_df
+
+class AnalyticsManagement:
+    API_VER = 'v3'
+    API_NAME = 'analytics'
+
+    def __init__(self, account_id, property_id, view_id):
+        credentials = google_auth.build_credentials()
+        self.analytics = googleapiclient.discovery.build(self.API_NAME, self.API_VER, credentials=credentials)
+        self.account_id = account_id
+        self.property_id = property_id
+        self.view_id = view_id
+
+    def list_accounts(self):
+        """returns a list of management.accountSummaries Resource"""
+        accounts = self.analytics.management().accounts().list().execute()
+        return accounts.get('items', [])
+
+    def list_account_users(self):
+        users = self.analytics.management().accountUserLinks().list(accountId=self.account_id).execute()
+        return users.get('items', [])
+
+    def list_account_filters(self):
+        filters = self.analytics.management().filters().list(accountId=self.account_id).execute() 
+        return filters.get('items', [])
+
+    def get_property(self):
+        """per default pre-defined property is returned"""
+        prop = self.analytics.management().webproperties().get(
+            accountId=self.account_id,
+            webPropertyId=self.property_id).execute()
+        return prop
+
+    def list_view_filters(self):
+        """per default view filters for pre-defined property and view are returned"""
+        view_filters = self.analytics.management().profileFilterLinks().list(
+            accountId=self.account_id,
+            webPropertyId=self.property_id,
+            profileId=self.view_id).execute()
+        return view_filters.get('items', [])
+
+    def handle_filters(self):
+        account_filters = self.list_account_filters()
+        filter_dict = {}
+        for idx, filter in enumerate(account_filters):
+            filter_ins = Filter(f_id=filter['id'], f_name=filter['name'], f_type=filter['type'], f_update=filter['updated'])
+            if filter['type'] == "SEARCH_AND_REPLACE":
+                filter_ins.details = filter['searchAndReplaceDetails']
+            elif filter['type'] == "INCLUDE":
+                filter_ins.details = filter["includeDetails"]
+            elif filter['type'] == "EXCLUDE":
+                filter_ins.details = filter["excludeDetails"]
+            elif filter['type'] == "LOWERCASE":
+                filter_ins.details = filter["lowercaseDetails"]
+            elif filter['type'] == "UPPERCASE":
+                filter_ins.details = filter["uppercaseDetails"]
+            elif filter['type'] == "ADVANCED":
+                filter_ins.details = filter["advancedDetails"]
+            else:
+                filter_ins.details = {'key': 'value'}
+            filter_dict["filter_{}".format(idx)] = filter_ins
+
+        return filter_dict
